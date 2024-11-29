d759f29f578a6425cfdfe25d871441ca8aab3262
David Julian Bustos Cortes <dbustos@unal.edu.co>
2024-11-29 03:01:32 -0500

    Optimize imports en board

--- reto_4/AndroidTicTacToe/app/src/main/java/com/example/androidtic_tac_toe/ui/game/components/GameBoard.kt
+++ reto_4/AndroidTicTacToe/app/src/main/java/com/example/androidtic_tac_toe/ui/game/components/GameBoard.kt
@@ -1,34 +1,20 @@
 package com.example.androidtic_tac_toe.ui.game.components
 
-import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.androidtic_tac_toe.R
-import com.example.androidtic_tac_toe.ui.game.GameUiState
 import com.example.androidtic_tac_toe.ui.game.Player
 import com.example.androidtic_tac_toe.ui.game.SquareState
 
